501b70727cb1ea60a9ace5f77b2f743038a6443f
hitmac <test@hitto.me>
2023-10-11 12:09:51 +0900

    update 3 files and create 1 file

--- src/main.js
+++ src/main.js
@@ -23,13 +23,24 @@ function sendFromButton() {
 
 function send() {
   if (!myMonocle) { console.error('no monocle found'); return; }
+  var max = document.getElementById("max").value;
+  var min = document.getElementById("min").value;
+  var int = document.getElementById("int").value;
+  var num = document.getElementById("num").value;
+  if (Number(min) >= Number(max)) { console.error('min >= max'); showOutput("plz min < max\n   "); return; }
   console.log(code);
-  myMonocle.repl(code)
+  sendcode = code;
+  sendcode = sendcode.replaceAll("{max}", max);
+  sendcode = sendcode.replaceAll("{min}", min);
+  sendcode = sendcode.replaceAll("{int}", int);
+  sendcode = sendcode.replaceAll("{num}", num);
+  console.log(sendcode);
+  myMonocle.repl(sendcode)
     .then(r => showOutput(r))
 }
 
 function showOutput(r) {
   console.log('raw return:', r)
   document.querySelector("#output").innerText
-    += r.replace('OK', '').slice(0, -3)
+    += r.replace('OK', '').slice(0, -3) // -3 to remove the \r\n
 }
