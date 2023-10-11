53fb231805d31097b94a594a12be03e5e5d43af7
hitmac <test@hitto.me>
2023-10-11 10:53:31 +0900

    fn send

--- src/main.js
+++ src/main.js
@@ -5,11 +5,26 @@ window.onload = _ => {
   document.querySelector("#connect").disabled = false
 }
 
+var code = "";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "code", true);
+xhr.onload = function () {
+    code = xhr.responseText;
+}
+xhr.send();
+
 function sendFromButton() {
   if (!myMonocle) { console.error('no monocle found'); return; }
   parsedcode = this.parentElement.children[1].value
   console.log('parsedcode:', parsedcode)
   myMonocle.repl(parsedcode)
+    .then(r => showOutput(r))
+}
+
+function send() {
+  if (!myMonocle) { console.error('no monocle found'); return; }
+  console.log(code);
+  myMonocle.repl(code)
     .then(r => showOutput(r))
 }
 
